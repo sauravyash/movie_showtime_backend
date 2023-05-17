b7640bffb2504ed9e1ee0ed683d3a4436713e40c
sauravyash <12910821+sauravyash@users.noreply.github.com>
2023-05-17 19:52:04 +1000

    comments

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,18 +6,22 @@ const theaters = require('../../data/theater_showtimes.json')
 
 const router = express.Router();
 
+// display frontend code in public folder
 router.all('/', (req, res) => {
   res.sendFile('index.html');
 });
 
+// retrieve the list of all movies
 router.get('/movies', (req, res) => {
   res.json(movies);
 });
 
+// search for a specific movie according to a keyword
 router.get('/movies/find/:keyword', (req, res) => {
   res.json(movies.filter(r => r.title.toLowerCase().includes(req.params.keyword.toLowerCase())));
 });
 
+// search for a specific movie according to the given ID
 router.get('/movie/:movieID', (req, res) => {
   const movie = movies.find(r => r.id === req.params.movieID)
   if (!movie) {
@@ -26,10 +30,12 @@ router.get('/movie/:movieID', (req, res) => {
   res.json(movie);
 });
 
+// retrieve the list of all theaters
 router.get('/theaters', (req, res) => {
   res.json(theaters);
 });
 
+// search for a specific theater according to the given ID
 router.get('/theater/:theaterID/', (req, res) => {
   const theater = theaters.find(t => t.id === req.params.theaterID);
   if (!theater) {
